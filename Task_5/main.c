12bc19ce5f85a3c8dace7df8c3aef60cd7928cdf
AyhamRatrout <arr90@case.edu>
2021-09-23 14:13:17 -0400

    First commit- task 5 done

---
+++ Task_5/main.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include<limits.h>
+
+char *trim(char toTrim[])
+{
+    char *end;
+
+    while(isspace((unsigned char)*toTrim))
+    {
+        toTrim++;
+    }
+
+    if(*toTrim == 0)
+    {
+        return toTrim;
+    }
+
+    end = toTrim + strlen(toTrim) - 1;
+
+    while(end > toTrim && isspace((unsigned char)*end))
+    {
+        end--;
+    }
+
+    end[1] = '\0';
+
+    return toTrim;
+}
+
+char *removeFirstTwoWords(char *userInput)
+{
+    int wordCounter = 0;
+    userInput = trim(userInput);
+
+    while(*userInput != '\0' && wordCounter != 2)
+    {
+        if(*userInput != ' ')
+        {
+            userInput++;
+        }
+        else
+        {
+            userInput = trim(userInput);
+            wordCounter++;
+        }
+    }
+
+    if(wordCounter < 2)
+    {
+        printf("Hey, don't try to tick me like that! Your input had less than three words and therefore there is no third word to be printed :(");
+    }
+    return userInput;
+}
+
+void thirdWord(char *userInput)
+{
+    userInput = removeFirstTwoWords(userInput);
+    char result[strlen(userInput)];
+    int i = 0;
+
+    while(*userInput != ' ')
+    {
+        result[i] = *userInput;
+        i++;
+        userInput++;
+    }
+    result[i] = '\0';
+    printf("%s\n", result);
+}
+
+void main()
+{
+    char userInput[sizeof(INT_MAX) / 2];
+
+    printf("Hello there fellow user. Please type something below so that I can return to you the third word...\n");
+
+    fgets(userInput, (INT_MAX / 2), stdin);
+
+    thirdWord(userInput);
+}
+
+
