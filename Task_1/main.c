fca2bcb94d3573800f532e126d58df1d7269c85a
AyhamRatrout <arr90@case.edu>
2021-10-03 15:56:50 -0400

    Task 1 - Complete the task

--- Task_1/main.c
+++ Task_1/main.c
@@ -1,7 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<unistd.h>
+#include<windows.h>
 
 int CAPACITY = 100000;
+int StartingPID = 0;
+
+typedef struct Task
+{
+    int priority;
+    int burst;
+    int PID;
+}Task;
 
 typedef struct Queue
 {
@@ -9,14 +19,24 @@ typedef struct Queue
     int size;
     int front;
     int rear;
-    int *elements;
+    Task *tasks[];
 }Queue;
 
-Queue * createQueue()
+Task *createTask()
+{
+    Task *T;
+    T = (Task *) malloc(sizeof(Task));
+    T->priority = rand() % 256;
+    T->burst = 1 + rand() % 100;
+    T->PID = StartingPID++;
+    return T;
+}
+
+Queue *createQueue()
 {
     Queue *Q;
     Q = (Queue *) malloc(sizeof(Queue));
-    Q->elements = (int *) malloc(sizeof(int)*CAPACITY);
+    //Q->tasks[CAPACITY] = (Task *) malloc(CAPACITY * sizeof(Task));
     Q->size = 0;
     Q->capacity = CAPACITY;
     Q->front = 0;
@@ -54,23 +74,23 @@ void dequeue(Queue *Q)
         Q->front++;
         if(Q->front==Q->capacity)
         {
-            Q->front=0;
+            Q->front=NULL;
         }
     }
     return;
 }
 
-int front(Queue *Q)
+Task *front(Queue *Q)
 {
     if(Q->size==0)
     {
         printf("Queue is Empty\n");
         exit(0);
     }
-    return Q->elements[Q->front];
+    return Q->tasks[Q->front];
 }
 
-void enqueue(Queue *Q,int element)
+void enqueue(Queue *Q, Task *T)
 {
     if(isFull(Q))
     {
@@ -84,7 +104,7 @@ void enqueue(Queue *Q,int element)
         {
             Q->rear = 0;
         }
-        Q->elements[Q->rear] = element;
+        Q->tasks[Q->rear] = T;
     }
     return;
 }
@@ -92,9 +112,57 @@ void enqueue(Queue *Q,int element)
 
 void main()
 {
-    Queue *arrayOfQueues[256];
-    for (int i = 0; i < 256; i++)
-    {
-        arrayOfQueues[i] = createQueue();
+    Task *CPU[1];
+    CPU[0] = NULL;
+    struct Queue *runQueue[256];
+    for(int i = 0; i < 256; i++){
+        runQueue[i] = createQueue();
+    }
+
+    int cycleTimePassed = 0;
+    int CPUCycleDone = 0;
+
+    //simulator starts here
+    for(int i = 0; i < 70; i++){
+
+        if(cycleTimePassed != 0 && (cycleTimePassed % 10) == 0){
+            CPUCycleDone = 1;
+        }
+        else{
+            CPUCycleDone = 0;
+        }
+        //creates 10 different tasks
+        for(int j = 0; j < 2; j++){
+            Task *T = createTask();
+            printf("New task created! Priority = %d \t Burst Time = %d \t PID = %d\n", T->priority, T->burst, T->PID);
+            enqueue(runQueue[T->priority], T);
+        }
+        cycleTimePassed++;
+        if(CPU[0] == NULL){
+            for(int j = 0; j < 256; j++){
+                if(isEmpty(runQueue[j]) != 1){
+                    CPU[0] = runQueue[j]->tasks[0];
+                    dequeue(runQueue[j]);
+                    break;
+                }
+            }
+            printf("\nProcess with PID %d and priority %d was just moved to the CPU. Remaining time to complete this process is: %d\n\n", CPU[0]->PID, CPU[0]->priority, CPU[0]->burst);
+        }
+        else{
+            CPU[0]->burst--;
+            printf("\nProcess with PID %d and priority %d is currently running on the CPU. Remaining time to complete this process is: %d\n\n", CPU[0]->PID, CPU[0]->priority, CPU[0]->burst);
+            if(CPU[0]->burst == 0){
+                printf("\nProcess with PID %d and priority %d is done executing! A new process will be added to the CPU in the next cycle.\n\n", CPU[0]->PID, CPU[0]->priority);
+                cycleTimePassed = 0;
+                CPU[0] = NULL;
+            }
+            else if(CPUCycleDone == 1){
+                enqueue(runQueue[CPU[0]->priority], CPU[0]);
+                printf("\nProcess with PID %d and priority %d has used up a full CPU cycle and is yet to be completed! This process will be kicked off the CPU and a new process will take its place in the next cycle.\n\n", CPU[0]->PID, CPU[0]->priority, CPU[0]->burst);
+                cycleTimePassed = 0;
+                CPU[0] = NULL;
+            }
+        }
+        //Sleep(250);
     }
 }
